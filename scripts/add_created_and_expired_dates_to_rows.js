1fdf733c35f10d06d6e7d3a773ee70a0bb4100f2
Paulo Bu <pbu_98@yahoo.com>
2015-09-13 14:13:08 +0200

    Adds script to update the schema with created and expiresAt

---
+++ scripts/add_created_and_expired_dates_to_rows.js
@@ -0,0 +1,26 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/diffy';
+
+function main() {
+    var now = new Date;
+    MongoClient.connect(url, function(err, db) {
+        var diffy = db.collection('diffy');
+        var cursor = diffy.find({}, {_id:1, created:1, expiresAt:1}).sort({created: 1});
+        cursor.each(function(err, doc) {
+            if (doc == null) {
+                db.close();
+                return;
+            }
+            if (!doc.created) {
+                console.log("not created: " + doc._id);
+                diffy.update({_id: doc._id}, {'$set' : {created: new Date}});
+            }
+            if (!doc.expiresAt) {
+                console.log("not expiresAt: " + doc._id);
+                diffy.update({_id: doc._id}, {'$set' : {expiresAt: new Date}});
+            }
+        });
+    });
+}
+
+main();
