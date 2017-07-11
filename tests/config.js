c36311d393076fab63b149d10953710f13507016
Paulo Bu <pbu_98@yahoo.com>
2017-07-11 16:45:31 +0200

    Fixes typo on the tests

--- tests/config.js
+++ tests/config.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect;
 var config = require('../src/config');
 
-describe('Diff', () => {
+describe('Config Tests', () => {
     it('should read analytics key from env', () => {
         expect(config.GA_ANALITYCS_KEY).to.be.equal('fake key');
     });
