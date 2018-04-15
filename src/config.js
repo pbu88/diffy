f70146bdd6ab90687866f87b40214458ce1c2583
Paulo Bu <paulobu88@gmail.com>
2018-04-15 19:02:49 +0200

    Use new classes within app.js

--- src/config.js
+++ src/config.js
@@ -5,7 +5,8 @@ config.port = parseInt(process.env.DIFFY_WEB_PORT) || 3000;
 
 config.db_host = process.env.DIFFY_DB_HOST || 'localhost';
 config.db_port = process.env.DIFFY_DB_PORT || 27017;
-config.db_url = `mongodb://${config.db_host}:${config.db_port}/diffy`;
+config.db_url  = `mongodb://${config.db_host}:${config.db_port}/diffy`;
+config.db_name = 'diffy';
 
 config.GA_ANALITYCS_KEY = process.env.DIFFY_GA_ANALYTICS_KEY;
 
