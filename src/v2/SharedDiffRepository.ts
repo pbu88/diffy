6f7ff21fece55acc497aada29f134d2b7fe9bb55
Paulo Bu <paulobu88@gmail.com>
2018-04-22 18:40:19 +0200

    Use random string gen as id instead of mongo default
    To keep backward compatibility and enforce randomness.

--- src/v2/SharedDiffRepository.ts
+++ src/v2/SharedDiffRepository.ts
@@ -1,5 +1,6 @@
 import mongodb = require('mongodb');
 import {  SharedDiff } from './SharedDiff';
+const utils = require('../utils.js').Utils;
 
 export interface SharedDiffRepository {
     insert: (diff: SharedDiff) => Promise<SharedDiff>;
@@ -31,16 +32,16 @@ export class MongoSharedDiffRepository implements SharedDiffRepository {
         return this.client
             .then(client => client.db(this.db_name))
             .then(db => db.collection(COLLECTION_NAME))
-            .then(collection => collection.insertOne(diff))
-            .then(result => result.insertedId)
-            .then(id => ({...diff, id: id.toHexString()}));
+            .then(collection => collection.insertOne({...diff, _id: utils.genRandomString()}))
+            .then(result => result.insertedId.toString())
+            .then(id => ({...diff, id }));
     }
 
     fetchById(id: string): Promise<SharedDiff> {
         return this.client
             .then(client => client.db(this.db_name))
             .then(db => db.collection(COLLECTION_NAME))
-            .then(collection => collection.findOne({"_id": new mongodb.ObjectID(id)}))
+            .then(collection => collection.findOne({"_id": id}))
             .then(doc => ({id: doc._id, ...doc}));
     }
 
@@ -49,7 +50,7 @@ export class MongoSharedDiffRepository implements SharedDiffRepository {
         return this.client
             .then(client => client.db(this.db_name))
             .then(db => db.collection(COLLECTION_NAME))
-            .then(collection => collection.deleteOne({"_id": new mongodb.ObjectID(id)}))
+            .then(collection => collection.deleteOne({"_id": id}))
             .then(result => result.deletedCount);
     }
 }
