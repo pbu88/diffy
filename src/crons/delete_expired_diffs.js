52d2e6d8e04b22ec042d97ff42e30b5eb09ef847
Paulo Bu <paulobu88@gmail.com>
2018-04-22 19:17:18 +0200

    Do not output on cron success, otherwise emails are sent

--- src/crons/delete_expired_diffs.js
+++ src/crons/delete_expired_diffs.js
@@ -8,7 +8,7 @@ function main() {
         var diffy = db.collection('diffy');
         diffy.deleteMany({expiresAt: {'$lte' : new Date}})
             .then(
-                (result) => { console.log(result.deletedCount + " garbage collected docs from mongo") },
+                (result) => { /* success, TODO: add metrics in the future */},
                 (err) => { console.error(err) })
             .then(
                 () => { db.close() });
