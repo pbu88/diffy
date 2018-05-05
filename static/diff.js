98b4e5f3649ef2f2513cf7d4d1d73ea1d243f69a
Paulo Bu <paulobu88@gmail.com>
2018-05-05 20:37:17 +0200

    Underline currently selected file

--- static/diff.js
+++ static/diff.js
@@ -5,12 +5,14 @@ $(function() {
 
     var $bigWrapper = $('.d2h-wrapper');
     $('.file').click(function() {
+        $('.file').removeClass("selected");
+        $(this).addClass("selected");
         $wrappers.hide();
         var filename = $(this).data('filename').slice(1);
         $diffFileWrapper = $bigWrapper.find(
                 '.d2h-file-wrapper:has(.d2h-file-name:contains(' + filename + '))').first().show();
     });
-    
+
     $('a.directory').click(function() {
         $this = $(this);
         $this.parent().toggleClass('directory-hidden');
