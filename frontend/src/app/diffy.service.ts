3112056aaf66c154b5a13af51a4f202da97d8c4a
Paulo Bu <paulobu88@gmail.com>
2018-11-16 19:08:20 +0100

    Whitespace only patch

--- frontend/src/app/diffy.service.ts
+++ frontend/src/app/diffy.service.ts
@@ -8,64 +8,64 @@ import { Diff2Html }               from 'diff2html';
 //var diff2html = require('diff2html');
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DiffyService {
-  private diffyUrl = '/api/diff/';  // URL to web api
+    private diffyUrl = '/api/diff/';  // URL to web api
 
-  private storage: {[id: number]: string} = {};
-  private idCount = 1;
+    private storage: {[id: number]: string} = {};
+    private idCount = 1;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+    constructor(
+        private http: HttpClient
+    ) { }
 
-  private makeSharedDiff(raw_diff: string, date: Date = new Date()): SharedDiff {
-    let expire_date = new Date();
-    expire_date.setDate(date.getDate() + 1);
-    return {
-      created: date,
-      expiresAt: expire_date,
-      diff: Diff2Html.getJsonFromDiff(raw_diff),
-      rawDiff: raw_diff,
-    };
-  }
+    private makeSharedDiff(raw_diff: string, date: Date = new Date()): SharedDiff {
+        let expire_date = new Date();
+        expire_date.setDate(date.getDate() + 1);
+        return {
+            created: date,
+            expiresAt: expire_date,
+            diff: Diff2Html.getJsonFromDiff(raw_diff),
+            rawDiff: raw_diff,
+        };
+    }
 
-  public submitDiff(diffText: string): Observable<number> {
-      this.storage[this.idCount] = diffText;
-      const id = this.idCount;
-      this.idCount++;
-      return of(id);
-  }
+    public submitDiff(diffText: string): Observable<number> {
+        this.storage[this.idCount] = diffText;
+        const id = this.idCount;
+        this.idCount++;
+        return of(id);
+    }
 
-  private handleError<T> (operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
+    private handleError<T> (operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
 
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
 
-      // TODO: better job of transforming error for user consumption
-      console.log(`${operation} failed: ${error.message}`);
+            // TODO: better job of transforming error for user consumption
+            console.log(`${operation} failed: ${error.message}`);
 
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
+        };
+    }
 
-  public getDiff(id: string): Observable<any> {
-    return this.http.get(this.diffyUrl + id).pipe(
-      catchError(this.handleError('getDiff', null))
-    );
-  }
+    public getDiff(id: string): Observable<any> {
+        return this.http.get(this.diffyUrl + id).pipe(
+            catchError(this.handleError('getDiff', null))
+        );
+    }
 
-  /** PUT: update the hero on the server */
-  storeDiff(diffText: string): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    };
+    /** PUT: update the hero on the server */
+    storeDiff(diffText: string): Observable<any> {
+        const httpOptions = {
+            headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+        };
 
-    return this.http.put(this.diffyUrl, {diff: diffText}, httpOptions).pipe(
-      catchError(this.handleError('getDiff', null))
-    );
-  }
+        return this.http.put(this.diffyUrl, {diff: diffText}, httpOptions).pipe(
+            catchError(this.handleError('getDiff', null))
+        );
+    }
 }
--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -3,36 +3,36 @@ import { DiffyService }             from '../diffy.service';
 import { Router }                   from '@angular/router';
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+    selector: 'app-home-page',
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  @Input() diffText: string;
+    @Input() diffText: string;
 
-  constructor(
-    private router: Router,
-    private diffyService: DiffyService
+    constructor(
+        private router: Router,
+        private diffyService: DiffyService
     ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  submitDiff() {
-  //  this.diffyService.submitDiff(this.diffText)
-  //    .subscribe(id => this.router.navigate([`/diff/${id}`]));
-    this.diffyService.storeDiff(this.diffText)
-      .subscribe(sharedDiff => this.router.navigate([`/diff/${sharedDiff.id}`]));
-  }
+    submitDiff() {
+        //  this.diffyService.submitDiff(this.diffText)
+        //    .subscribe(id => this.router.navigate([`/diff/${id}`]));
+        this.diffyService.storeDiff(this.diffText)
+            .subscribe(sharedDiff => this.router.navigate([`/diff/${sharedDiff.id}`]));
+    }
 
-  uploadChange(fileInput: Event){
-    let file = (fileInput.target as any).files[0];
-    let reader = new FileReader();
-    reader.onload = (e) => {
-        this.diffText = (e.target as any).result
-        this.submitDiff();
-    };
-    reader.readAsText(file);
-  }
+    uploadChange(fileInput: Event){
+        let file = (fileInput.target as any).files[0];
+        let reader = new FileReader();
+        reader.onload = (e) => {
+            this.diffText = (e.target as any).result
+            this.submitDiff();
+        };
+        reader.readAsText(file);
+    }
 
 }
