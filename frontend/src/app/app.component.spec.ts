ef0b84cab517cb65ac2c4732b362cee1530ea291
Paulo Bu <paulobu88@gmail.com>
2022-01-21 00:03:03 +0100

    Track single page application pageviews in GA

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import {async, TestBed} from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import {AppComponent} from './app.component';
 
@@ -6,6 +7,7 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed
         .configureTestingModule({
+          imports: [RouterTestingModule],
           declarations: [AppComponent],
         })
         .compileComponents();
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,19 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component(
-    {selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
+  { selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css'] })
 export class AppComponent {
   title = 'Diffy';
 
-  constructor() {}
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        (<any>window).ga('set', 'page', event.urlAfterRedirects);
+        (<any>window).ga('send', 'pageview');
+      });
+  }
 }
