ee980d0ae2331647f6e31d012292839a61af792b
capo999 <alexrod211099@gmail.com>
2022-02-05 23:22:42 -0600

    Move footer and header into layout folder

---
+++ frontend/src/app/components/layout/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
