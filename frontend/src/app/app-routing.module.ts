42e52acfbbbd8a59f890cd1461760c44a07c51b8
capo999 <alexrod211099@gmail.com>
2022-02-05 23:24:26 -0600

    Apply separate layouts to each route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,22 +1,37 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { DiffyLayoutComponent } from './components/layout/diffy-layout/diffy-layout.component';
+import { SiteLayoutComponent } from './components/layout/site-layout/site-layout.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import {DiffDetailComponent} from './pages/diff-detail/diff-detail.component';
-import {HomePageComponent} from './pages/home-page/home-page.component';
-
+import { DiffDetailComponent } from "./pages/diff-detail/diff-detail.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent, pathMatch: 'full'},
-  {path: 'diff/:id', component: DiffDetailComponent},
+  { 
+    path: '', 
+    component: SiteLayoutComponent, 
+    children: [{
+      path: '',
+      component: HomePageComponent,
+      pathMatch: "full"
+    }]
+  },
+  { 
+    path: '', 
+    component: DiffyLayoutComponent, 
+    children: [{
+      path: 'diff/:id',
+      component: DiffDetailComponent,
+    }]
+  },
 
   // Match all and reditect to homepage
-  {path: '**', redirectTo: ''},
+  { path: "**", redirectTo: "" },
 ];
 
 @NgModule({
   exports: [RouterModule],
   imports: [RouterModule.forRoot(routes)],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,22 @@
-import {HttpClientModule} from '@angular/common/http';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+import { CommonModule } from "@angular/common";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {DiffDetailContentComponent} from './components/diff-detail-content/diff-detail-content.component';
-import {DiffDetailCountdownComponent} from './components/diff-detail-countdown/diff-detail-countdown.component';
-import {DiffDetailNavComponent} from './components/diff-detail-nav/diff-detail-nav.component';
-import {DiffDetailComponent} from './pages/diff-detail/diff-detail.component';
-import {DiffFileTreeComponent} from './components/diff-file-tree/diff-file-tree.component';
-import {HighlightComponent} from './components/highlight/highlight.component';
-import {HomePageComponent} from './pages/home-page/home-page.component';
-import {EscapeHtmlPipe} from './pipes/keep-html.pipe';
-import { FooterComponent } from './components/footer/footer.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { DiffDetailContentComponent } from "./components/diff-detail-content/diff-detail-content.component";
+import { DiffDetailCountdownComponent } from "./components/diff-detail-countdown/diff-detail-countdown.component";
+import { DiffDetailNavComponent } from "./components/diff-detail-nav/diff-detail-nav.component";
+import { DiffDetailComponent } from "./pages/diff-detail/diff-detail.component";
+import { DiffFileTreeComponent } from "./components/diff-file-tree/diff-file-tree.component";
+import { HighlightComponent } from "./components/highlight/highlight.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { EscapeHtmlPipe } from "./pipes/keep-html.pipe";
+import { FooterComponent } from "./components/layout/footer/footer.component";
+import { HeaderComponent } from "./components/layout/header/header.component";
 import { DiffyLayoutComponent } from './components/layout/diffy-layout/diffy-layout.component';
 import { SiteLayoutComponent } from './components/layout/site-layout/site-layout.component';
 
@@ -31,17 +32,18 @@ import { SiteLayoutComponent } from './components/layout/site-layout/site-layout
     DiffDetailCountdownComponent,
     HighlightComponent,
     FooterComponent,
+    HeaderComponent,
     DiffyLayoutComponent,
     SiteLayoutComponent,
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     HttpClientModule,
-    AppRoutingModule,
     FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
